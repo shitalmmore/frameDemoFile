1a0303972d6ac3db6190c23b9783acf6e1cee659
shitalmmore <shitalmarotimoregmail.com>
2023-07-12 12:48:53 +0530

    new file add

---
+++ src/seleniumRivision/faceBook.java
@@ -0,0 +1,24 @@
+package seleniumRivision;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class faceBook {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Amar\\Desktop\\chromedriver_win32new\\chromedriver.exe");
+		WebDriver wb=new ChromeDriver();
+		
+		wb.get("https://www.facebook.com/login/");
+		wb.manage().window().maximize();
+		
+		
+		
+		
+		
+		
+	}
+
+}
